0d846a5cd51472d7fe7bbc1829211a9f8d2b30f0
mushan <mushan.zbge@google.com>
2019-10-24 23:53:11 +0800

    初步完成项目建设，需要进行部分细节优化
    Change-Id: I9bb6477374fd3f6420ec40666d5947d905552c8f

---
+++ README.md
@@ -0,0 +1,19 @@
+# 天气预报 - weather
+一款天气应用微信小程序
+
+## 说明
+
+I、气象数据由[和风天气](http://www.heweather.com/)，需要注册账号获取 `key`；免费版只能获取三天的天气数据，若要获取七天的气象数据，可以申请个人开发者认证；
+
+## 运行前准备
+> * [注册微信小程序](https://mp.weixin.qq.com/wxopen/waregister?action=step1)，获取 `appid`，配置域名白名单(在小程序后台将使用到的 `API` 添加到域名白名单)；
+> * ~~注册[百度地图开放平台](https://lbsyun.baidu.com/)开发者，创建应用 **（注意：应用类型选择微信小程序时，请填写真实的小程序 appid）** 
+> * 注册[和风天气](http://www.heweather.com/)账号，获取 'key`；
+> * 在 `app.js` 中替换 `globalData` 中的 `key` 为你的 `key`；
+> * Run and Enjoy!
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2018-present, 木杉
---
+++ pages/about/about.js
@@ -0,0 +1,60 @@
+let utils = require('../../utils/utils')
+Page({
+  data: {
+    projectAddress: 'https://github.com/zbge/weather',
+    github: 'https://github.com/zbge',
+    email: 'mushan.zbge@google.com',
+    qq: '642123650',
+    swiperHeight: 'auto',
+  },
+  onLoad () {
+    this.initSwiper()
+  },
+  previewImages (e) {
+    let index = e.currentTarget.dataset.index || 0
+    let urls = this.data.bannerImgList
+    let arr = []
+    let imgs = urls.forEach(item => {
+      arr.push(item.src)
+    })
+    wx.previewImage({
+      current: arr[index],
+      urls: arr,
+      success: function (res) { },
+      fail: function (res) {
+        console.error('previewImage fail: ', res)
+      }
+    })
+  },
+  initSwiper () {
+    let systeminfo = getApp().globalData.systeminfo
+    if (utils.isEmptyObject(systeminfo)) {
+      wx.getSystemInfo({
+        success: (res) => {
+          this.setSwiperHeight(res)
+        },
+      })
+    } else {
+      this.setSwiperHeight(systeminfo)
+    }
+  },
+  setSwiperHeight (res) {
+    this.setData({
+      swiperHeight: `${(res.windowWidth || res.screenWidth) / 375 * 200}px`
+    })
+  },
+  copy(e) {
+    let dataset = (e.currentTarget || {}).dataset || {}
+    let title = dataset.title || ''
+    let content = dataset.content || ''
+    wx.setClipboardData({
+      data: content,
+      success () {
+        wx.showToast({
+          title: `已复制${title}`,
+          duration: 2000,
+        })
+      },
+    })
+  },
+})
